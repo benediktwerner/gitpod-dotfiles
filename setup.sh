0cd4f7a5b18b18d1f9825e6b023d27e30d1b7eec
Benedikt Werner <1benediktwerner@gmail.com>
2023-01-05 03:33:14 +0100

    Move all dotfiles in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -eu;
+set -eu
+shopt -s dotglob
 
 if test ! -e /ide/bin/gitpod-code || test ! -v GITPOD_REPO_ROOT; then {
     printf 'error: This script is meant to be run on Gitpod, quiting...\n' && exit 1;
 } fi
 
 _source_dir="$(readlink -f "$0")" && _source_dir="${_source_dir%/*}";
-mv "$_source_dir/.gitconfig" "$HOME/.gitconfig"
-mv "$_source_dir/.gitignore" "$HOME/.gitignore"
+mv "$_source_dir/dotfiles/*" "$HOME/"
